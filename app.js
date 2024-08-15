ca3d436dbd89c5cf2376ddf62f78ec7f0f61e2b6
Rajbabu K Sah <157727710+Razzsha@users.noreply.github.com>
2024-08-15 14:24:14 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newGamebtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-cont");
+let msg = document.querySelector(".msg");
+
+let turnO = true;
+
+let movecount = 0; //counter for track numbers of moves
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+const resetGame = () => {
+    turnO = true;
+    movecount = 0; //Reset move counter 
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        // console.log("box was clicked");
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        movecount++;// increment move counter 
+        checkWinner();
+
+    });
+});
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+
+};
+
+
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+};
+
+const showDraw = () => {
+    msg.innerText = ` Match is Draw! `;
+    msgContainer.classList.remove("hide");
+};
+
+
+const checkWinner = () => {
+    for (pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("winner", pos1Val);
+
+                showWinner(pos1Val);
+                disableAllBoxes();
+                return;
+            }
+        }
+
+    }
+    // if 9 moves have been made and no winner, match is draw!
+    if (movecount === 9) {
+        showDraw();
+        disableAllBoxes();
+
+    }
+
+
+};
+
+const disableAllBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+};
+
+newGamebtn.addEventListener("click", resetGame);
+resetbtn.addEventListener("click", resetGame);
